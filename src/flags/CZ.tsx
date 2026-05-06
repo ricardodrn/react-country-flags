f9776786b5eb9c648defc7cc7461df23ed6c0d78
Ricardo Nain <rreyesnain@gmail.com>
2026-05-06 13:50:40 -0400

    feat: Add SVG flag components for Zambia (ZM) and Zimbabwe (ZW)

---
+++ src/flags/CZ.tsx
@@ -0,0 +1,3 @@
+import type { SVGProps } from "react";
+const CZ = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480" {...props}><path fill="#fff" d="M0 0h640v240H0z" /><path fill="#d7141a" d="M0 240h640v240H0z" /><path fill="#11457e" d="M360 240 0 0v480z" /></svg>;
+export default CZ;
---
+++ src/flags/PL.tsx
@@ -0,0 +1,3 @@
+import type { SVGProps } from "react";
+const PL = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480" {...props}><g fillRule="evenodd"><path fill="#fff" d="M640 480H0V0h640z" /><path fill="#dc143c" d="M640 480H0V240h640z" /></g></svg>;
+export default PL;
